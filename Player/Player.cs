446e38fb4a91d3ae556066ce942a9cbc5bd7491d
Zurisen <carlosmarctor@gmail.com>
2025-06-15 18:28:31 +0200

    added camera rotation pivots

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,14 +1,30 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Player : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
+	[Export]
+	public float MouseSensitivity = 0.00075f;
+	private Vector2 _look = Vector2.Zero;
+	private Node3D _horizontalPivot;
+	private Node3D _verticalPivot;
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		_horizontalPivot = GetNode<Node3D>("HorizontalPivot");
+		_verticalPivot = GetNode<Node3D>("HorizontalPivot/VerticalPivot");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
+		_handleCameraRotation();
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -39,5 +55,31 @@ public partial class Player : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		base._UnhandledInput(@event);
+		if (Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			if (@event is InputEventMouseMotion mouseMotion)
+			{
+				_look = -mouseMotion.Relative * MouseSensitivity;
+			}
+		}
+	}
+
+	private void _handleCameraRotation()
+	{
+		var smoothCameraArm = GetNode<SpringArm3D>("SmoothCameraArm");
+		if (smoothCameraArm == null || _horizontalPivot == null || _verticalPivot == null) return;
+
+		_horizontalPivot.RotateY(_look.X);
+		_verticalPivot.RotateX(_look.Y);
+
+		float newPitch = (float)Mathf.Clamp(_verticalPivot.Rotation.X, -Math.PI / 2f, Math.PI / 2f);
+		smoothCameraArm.Rotation = new Vector3(newPitch, _horizontalPivot.Rotation.Y, 0);
+
+		_look = Vector2.Zero;
 	}
 }
