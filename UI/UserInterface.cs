2609cda1994be646691069712d3e124d22d6899f
Zurisen <carlosmarctor@gmail.com>
2025-07-31 18:35:04 +0200

    feat: Implement inventory system; add inventory pause toggle functionality and UI integration

--- UI/UserInterface.cs
+++ UI/UserInterface.cs
@@ -9,6 +9,7 @@ public partial class UserInterface : Control
     private Label _levelLabel;
     private TextureProgressBar _healthBar;
     private Label _healthLabel;
+    private Inventory _inventory;
 
     public override void _Ready()
     {
@@ -16,6 +17,23 @@ public partial class UserInterface : Control
         _levelLabel = GetNode<Label>("MarginContainer/InfoBar/HealthBarOver/LevelLabel");
         _healthBar = GetNode<TextureProgressBar>("MarginContainer/InfoBar/HealtBarUnder/HealthBar");
         _healthLabel = GetNode<Label>("MarginContainer/InfoBar/HealtBarUnder/HealthBar/HealthLabel");
+        _inventory = GetNode<Inventory>("Inventory");
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+        if (@event.IsActionPressed("open_inventory"))
+        {
+            _handleInventory();
+        }
+    }
+
+    private void _handleInventory()
+    {
+        _inventory.Visible = !_inventory.Visible;
+        Input.MouseMode = _inventory.Visible ? Input.MouseModeEnum.Visible : Input.MouseModeEnum.Captured;
+        GetTree().Paused = _inventory.Visible;
     }
 
     public void Init()
@@ -23,6 +41,7 @@ public partial class UserInterface : Control
         UpdateLevelLabel();
         UpdateHealthBar();
     }
+
     public void UpdateHealthBar()
     {
         GD.Print("Triggered: ", _player.HealthComponent.CurrentHealth);
@@ -30,7 +49,7 @@ public partial class UserInterface : Control
         _healthBar.Value = _player.HealthComponent.CurrentHealth;
 
         _healthLabel.Text = $"{_healthBar.Value} / {_healthBar.MaxValue}";
-        
+
     }
 
     public void UpdateLevelLabel()
